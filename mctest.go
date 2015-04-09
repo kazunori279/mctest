9ccaae7ea18ed41ea435dff1ca781b619c5615f2
Kazunori Sato <kazunori279@gmail.com>
2015-04-09 21:57:36 +0900

    Implemented time measurement

--- mctest.go
+++ mctest.go
@@ -3,16 +3,57 @@ package main
 import (
 	"fmt"
 	"os"
-	"github.com/bradfitz/gomemcache/memcache"
+	"bytes"
+	"time"
+    "crypto/rand"
+    "encoding/base64"
+    "github.com/bradfitz/gomemcache/memcache"
+    "github.com/montanaflynn/stats"
 )
 
 func main() {
 	mc := memcache.New("localhost:11211")
-	mc.Set(&memcache.Item{Key: "foo", Value: []byte("my value")})
-	it, err := mc.Get("foo")
+	n := 10000
+	timeToSet := make([]float64, n)
+	timeToGet := make([]float64, n)
+	for i := 0; i < n; i++ {
+		timeToSet[i], timeToGet[i] = measureSetAndGetTime(mc)
+	}
+	fmt.Printf("Set min: %v, max: %v, 90%%: %v, 99%%: %v \n", 
+		stats.Min(timeToSet), stats.Max(timeToSet),
+		stats.Percentile(timeToSet, 90), stats.Percentile(timeToSet, 99))
+}
+
+func measureSetAndGetTime(mc *memcache.Client) (float64, float64) {
+
+	// create key and value
+	k := randStr(10)
+	v := []byte(randStr(1024))
+
+	// test setting
+	start := time.Now().Nanosecond()
+	mc.Set(&memcache.Item{Key: k, Value: v})
+	timeToSet := float64((time.Now().Nanosecond() - start)) / 1000.0
+
+	// test getting
+	start = time.Now().Nanosecond()
+	it, err := mc.Get(k)
         if err != nil {
 		fmt.Printf("%v\n", err)
 		os.Exit(1)
 	}
-	fmt.Printf("%v\n", it)
+	timeToGet := float64((time.Now().Nanosecond() - start)) / 1000.0
+
+	// validate the value
+	if !bytes.Equal(it.Value, v) {
+		fmt.Printf("Wrong value: %v\n", it.Value)
+		os.Exit(1)
+	}
+	return timeToSet, timeToGet
+}
+
+func randStr(l int) string {
+	v := make([]byte, l)
+	rand.Read(v)
+	return base64.URLEncoding.EncodeToString(v)
 }
