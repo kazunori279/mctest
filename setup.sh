d7c053c4a132b2d5f405692ac5672bd2119f56c0
Kazunori Sato <kaz@sth.co.jp>
2015-05-06 15:44:43 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Default setups
+#
+
+# install default tools
+sudo apt-get update
+sudo apt-get -y install tmux git curl 
+
+# init git
+curl https://raw.githubusercontent.com/kazunori279/dotfiles/master/gitconfig -o ~/.gitconfig
+mkdir ~/git
+git init
+
+# init go
+mkdir ~/go
+curl https://raw.githubusercontent.com/kazunori279/dotfiles/master/bash_profile -o ~/.bash_profile
+curl https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz -o /tmp/golang.tar.gz
+sudo tar -C /usr/local -xzf /tmp/golang.tar.gz
+source ~/.bash_profile
+
+# install mctest
+cd ~/git
+git clone https://github.com/kazunori279/mctest.git
+go get github.com/bradfitz/gomemcache/memcache
+go get github.com/montanaflynn/stats
